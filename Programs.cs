2f08526d5902ed2971576b9607ba0b89fa32af8d
sandeep <c0739443@mylambton.ca>
2019-07-17 11:04:55 -0400

    linked

---
+++ Programs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new LinkedList();
+            a.CreateLikedList();
+        }
+    }
+}
+
+class Node
+{
+   public  Node nextlink;
+   public  Node previousLink;
+    public String nodeName;
+}
+class LinkedList
+{
+    Node Head, Orange, Tangerine, Tail;
+    public void CreateLikedList()
+    {
+        Head = new Node();
+        Orange = new Node();
+        Tangerine = new Node();
+        Tail = Tangerine;
+        Head.nextlink = Orange;
+        Console.WriteLine("hello");
+
+    }
+}
